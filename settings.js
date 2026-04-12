8c3331198f2de5b8578176b049ef525b7a9b543f
Zacharia316 <zappyblues234@gmail.com>
2026-04-12 23:02:43 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,26 @@
+/*
+𝘽𝘼𝙎𝙀 𝘽𝙔 𝙅𝘼𝙈𝙀𝙎𝙏𝙀𝘾𝙃 𝙄𝙉𝘾
+
+
+𝙩𝙮𝙥𝙚 : 𝙩𝙚𝙡𝙚𝙭𝙬𝙖
+
+𝙨𝙤𝙧𝙘𝙚 : 𝙟𝙖𝙢𝙚𝙨𝙙𝙚𝙫
+
+𝙩𝙜 : https://t.me/jamesBotz3
+
+𝙚𝙧𝙧𝙤𝙧𝙨 𝙛𝙞𝙭 : +254704955033
+
+𝙛𝙤𝙧 𝙢𝙤𝙧𝙚 𝙗𝙖𝙨𝙚𝙨 𝙟𝙤𝙞𝙣 𝙤𝙪𝙧 𝙩𝙚𝙡𝙚𝙜𝙧𝙖𝙢 𝙘𝙝𝙖𝙣𝙣𝙚𝙡
+*/
+
+module.exports = {
+  TELEGRAM_TOKEN: '8771469569:AAEZl9geia-_G6kkmQrSJ3FHMsHmLCkysOQ',
+  OWNER_ID: 6817804856,
+  BOT_NAME: 'NOVA-XMD™©®✓',
+  DEFAULT_PREFIX: '.',
+  SESSIONS_DIR: './sessions',
+  PREMIUM_DB: './database/premium.json',
+  MENU_IMAGE: 'https://i.imgur.com/yourimage.jpg',
+  REPORT_CHAT: 6817804856,
+  ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY || '',
+};
